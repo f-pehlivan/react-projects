e5cf77c4f84bd9465a196499ad9d314ce25effd4
f-pehlivan <1pehlivanfaruk@gmail.com>
2022-05-05 11:37:25 +0200

    movies mapped in MovieCard

--- Movie-App/movie-app/src/pages/Main.js
+++ Movie-App/movie-app/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState  } from 'react'
+import MovieCard from '../components/MovieCard'
 
 const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f&query='
@@ -8,6 +9,7 @@ const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=d6278b3dc3
 const Main = () => {
 
   const [ movies, setMovies ] = useState([])
+  const [searchTerm, setSearchTerm] = useState()
 
   useEffect(() => {
     getMovies(FEATURED_API)
@@ -21,9 +23,20 @@ const getMovies = (API) => {
 
 
   return (
-    <div>
-      {movies.map((movie) => <p>{movie.title}</p>)}
+
+    <>
+    <form className='search'>
+      <input type='search'
+        className='search-input'
+        placeholder='Search a movie...'
+        value={searchTerm} />
+
+    </form>
+
+    <div className='movie-container'>
+        {movies.map((movie) => <MovieCard key={movie.id} {...movie} />)}
     </div>
+    </>
   )
 }
 
