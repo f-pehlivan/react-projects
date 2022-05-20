b42821854b33dd6f65eac11fd8f439b53f47a5c1
f-pehlivan <1pehlivanfaruk@gmail.com>
2022-05-20 08:45:53 +0200

    Realtime Database via Firebase created

---
+++ FireBlog-App/fireblog-app/src/contexts/AuthContextProvider.js
@@ -0,0 +1,19 @@
+import React, {useContext, createContext} from 'react'
+
+//Create context for autentication data
+const AuthContext = createContext();
+
+// Defina a function to get data from Auth context
+function useAuth(){
+  return useContext(AuthContext);
+},
+
+const AuthContextProvider = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default AuthContextProvider
